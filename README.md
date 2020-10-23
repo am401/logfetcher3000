7a54651ebc52f164aec7fc407a0e76cd3a356fd5
am401 <andras.marton@wpengine.com>
2020-10-23 00:09:30 -0500

    Updated main application to allow for scalability of the number of domains provided

--- README.md
+++ README.md
@@ -2,20 +2,34 @@
 LogFetcher3000 is a Python based tool to retrieve access and error logs from WP Engine hosted websites that utilize the WP Engine Must Use plugins.
 
 # Usage
-This tool uses a JSON file to hold the current access and current error log for the site in question. The current version only handles a single set of links and is in the following format:
+This tool uses a JSON file to hold the current access and current error log for the site in question. Links should be added as lists within the dictionary: 
 
 ```
-{"access": "https://mysite.com/link/to/access.log", "error": "https://mysite.com/link/to/error.log"}
+{"access": ["https://mysite.com/link/to/access.log"], "error": ["https://mysite.com/link/to/error.log", "https://myothersite.com/link/to/error.log"]}
 ```
 
 The links can be obtained from within the `WP Admin Dashboard` within the WPE MU Plugin and inserted in a file called `links.json`.
 
-# Requirements
-I haven't currently made a `requirements.txt` file, however `requests` will need to be installed for the script to work.
+# cron
+This script can be called by a cron at a specific time  to download the day's logs. For example to grab the logs on the hour every hour:
+
+```
+0 * * * * cd /path/to/logfetcher3000 && /usr/local/bin/python3 logfetcher3000.py
+```
 
 # Change Log
 All notable changes to this project will be documented in this section.
 
+## [0.2] - 2020-10-22
+### Changed
+- Set the D_STAMP global variables to uppercase
+- Moved opening the JSON file under __main__
+- Handling the file under get_log() to be with a content manager
+- Generated filenames are now in the format of `environmentname_[access|error]_datestamp.log`
+### Added
+- Support for dynamically reading each dictionary value by looping through the list provided in `links.json`
+- Regex library to get the environment name from error log link
+- Created requirements.txt
 ## [0.1] - 2020-09-26
 ### Added
 - Ability to add links to a JSON file to be read by the script
--- logfetcher3000.py
+++ logfetcher3000.py
@@ -1,24 +1,43 @@
 import datetime
 import json
+import re
 import requests
 
-# Generate datestamp uisng date and time in order to create unique file names
-d_stamp = datetime.datetime.today()
-d_stamp = d_stamp.strftime('%m%d%Y_%H%M%S')
-
-# Set links within a JSON string to be utilized
-with open('links.json') as json_file:
-    url_json_object = json.load(json_file)
+# Generate datestamp for unique file names
+D_STAMP = datetime.datetime.today()
+D_STAMP = D_STAMP.strftime('%m%d%Y_%H%M%S')
 
 
 def get_log(url, filename):
+    """
+    Function to download the log files and save them locally.
+    Set User-Agent when accessing websites to LogFetcher3000.
+    :param url: list
+    :param filename: string
+    """
     headers = {'User-Agent': 'LogFetcher3000'}
     r = requests.get(url, allow_redirects=True, headers=headers)
-    open(filename, 'wb').write(r.content)
+    with open(filename, 'wb') as f:
+        f.write(r.content)
 
 
 if __name__ == '__main__':
-    domain = url_json_object["access"].split('/')
-    log_filename = domain[2].replace(".", "_")
-    get_log(url_json_object["access"], '{}_{}.log'.format(log_filename, d_stamp))
-    get_log(url_json_object["error"], '{}_error_{}.log'.format(log_filename, d_stamp))
+    with open('links.json') as json_file:
+        url_json_object = json.load(json_file)
+    access_links = url_json_object["access"]
+    error_links = url_json_object["error"]
+    """
+    Loop through each list under the given key in the dict.
+    This allows for scalability of the script.
+    Using the if statements in case the dict value is empty.
+    """
+    for i in range(len(access_links)):
+        if access_links[i]:
+            log_filename = access_links[i].split('/')
+            log_filename = log_filename[3]
+            get_log(access_links[i], '{}_access_{}.log'.format(log_filename, D_STAMP))
+    for i in range(len(error_links)):
+        if error_links[i]:
+            log_filename = re.findall('.*account_name=([^&]*)', error_links[i])
+            log_filename = log_filename[0]
+            get_log(error_links[i], '{}_error_{}.log'.format(log_filename, D_STAMP))
